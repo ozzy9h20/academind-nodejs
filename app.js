50ffef5562d5f263e0cecbb2782b4f9095a57c0d
Mochammad Endyarroziqin <mynameis_ozzy@yahoo.com>
2021-09-23 13:09:00 +0700

    Redirecting requests & write dummy file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
+  const {url, method} = req;
 
   if (url === '/') {
     res.write('<html>');
@@ -16,6 +17,13 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
