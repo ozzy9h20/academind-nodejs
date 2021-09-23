4a6ff66d27c1027e5fee17b75193f6030d261e57
Mochammad Endyarroziqin <mynameis_ozzy@yahoo.com>
2021-09-23 13:44:37 +0700

    [Refactor] to Non-Blocking code

--- app.js
+++ app.js
@@ -24,15 +24,16 @@ const server = http.createServer((req, res) => {
       body.push(chunk);
     });
 
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const [, message] = parsedBody.split("=");
-      fs.writeFileSync('message.txt', message);
-    });
 
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
   }
 
   res.setHeader('Content-Type', 'text/html');
