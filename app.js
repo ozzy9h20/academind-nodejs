339a87ad2c953cef87ade3d1e8d9619df136171f
Mochammad Endyarroziqin <mynameis_ozzy@yahoo.com>
2021-09-23 16:35:56 +0700

    Handling "/" route - welcome

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('Server is running...');
+  const url = req.url;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+      <html>
+        <head>
+          <title>Assignment 1</title>
+        </head>
+        <body>
+          <p>Welcome to my page!</p>
+        </body>
+      </html>
+    `);
+    return res.end();
+  }
+
+  if (url === '/users') {
+    
+  }
+
 });
 
 server.listen(3000);
