debfa3b9f3b738ff0dab601217207554d7098a9d
Mochammad Endyarroziqin <mynameis_ozzy@yahoo.com>
2021-09-23 12:49:49 +0700

    Sending Responses

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  process.exit(); // Exit node event loop
+  // process.exit(); // Exit node event loop
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
