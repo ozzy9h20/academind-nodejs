5384feb9cbf54c0aec78eebb3eaefc5f7a8ff9fa
Mochammad Endyarroziqin <mynameis_ozzy@yahoo.com>
2021-09-23 16:41:08 +0700

    Handling "/users" route

--- app.js
+++ app.js
@@ -19,7 +19,22 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/users') {
-    
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+      <html>
+        <head>
+          <title>Assignment 1</title>
+        </head>
+        <body>
+          <ul>
+            <li>User 1</li>
+            <li>User 2</li>
+            <li>User 3</li>
+          </ul>
+        </body>
+      </html>
+    `);
+    return res.end();
   }
 
 });
