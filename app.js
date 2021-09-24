3d90758f358513b44133ab5979e0f881c7d43479
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-09-24 18:40:59 +0700

    Add universal middleware

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use('/', (req, res, next) => {
+  console.log('Always On!');
+  next();
+});
+
+app.use('/', (req, res, next) => {
   res.send('Test');
 });
 
