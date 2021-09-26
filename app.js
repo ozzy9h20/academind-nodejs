7047c7520f791bb9a7fab8207b827d0c95844dd0
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-09-27 05:48:22 +0700

    Parsing incoming request with body-parser

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always run!');
-  next();
-});
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title" />
+      <button type="submit">Send</button>
+    </form>`);
 });
+
+app.use('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+})
 
 app.use('/', (req, res, next) => {
   console.log('In another middleware');
