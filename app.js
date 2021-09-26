2207b923042ee55b31de238652ff84040974c9a8
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-09-27 06:49:23 +0700

    Add 404 page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
@@ -10,7 +11,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).send('<h1>404: Page not found!</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
