c500bfda6150d3e7e0a73ce968f165cc98f7f1dc
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-09-27 07:43:45 +0700

    Express.static middleware to serving css static files

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
