ec609ef3dadad508377d3b8b208cfc8bc840a6d7
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-09-27 14:25:35 +0700

    Using pug

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
