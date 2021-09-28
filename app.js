b43c139c57b86951e479a33f279d6e29ebde6151
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-09-28 14:05:52 +0700

    Handling main routes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const mainRouter = require('./routes/mainRouter');
+
 const app = express();
 
 app.set('port', process.env.port || 3000);
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/', (req, res, next) => {
-  res.send('<h1>Hello world<h1>');
-})
+app.use(mainRouter);
 
 app.listen(app.get('port'), server => {
   console.info(`Server listen on port ${app.get('port')}`);
