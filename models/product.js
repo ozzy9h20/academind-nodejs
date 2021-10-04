b80b19e85a23ec68678bf643af1ce7b3f5a89f59
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-10-04 17:48:20 +0700

    Refactor Product Model

--- models/product.js
+++ models/product.js
@@ -1,25 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const products = [];
-
 const p = path.join(
   path.dirname(process.mainModule.filename), 
   'data', 
   'products.json'
 );
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+}
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      console.log(fileContent);
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -28,11 +31,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 }
