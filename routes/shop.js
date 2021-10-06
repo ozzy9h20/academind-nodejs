65d9ed7fda131fccc061c30a619cf3b973a93489
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-10-06 11:26:40 +0700

    Passing data with POST req

--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,7 @@ const {
   getCheckout,
   getOrders,
   getProduct,
+  postCart,
 } = require("../controllers/shop");
 const router = express.Router();
 
@@ -13,6 +14,7 @@ router.get('/', getIndex);
 router.get('/products', getProducts);
 router.get("/products/:productId", getProduct);
 router.get('/cart', getCart);
+router.post('/cart', postCart)
 router.get('/orders', getOrders)
 router.get('/checkout', getCheckout);
 
