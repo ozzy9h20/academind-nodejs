9472f959bba401d4d70b9f7d982c1ab6935c4451
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-10-06 09:57:04 +0700

    Loading product detail data

--- routes/shop.js
+++ routes/shop.js
@@ -5,11 +5,13 @@ const {
   getCart,
   getCheckout,
   getOrders,
+  getProduct,
 } = require("../controllers/shop");
 const router = express.Router();
 
 router.get('/', getIndex);
 router.get('/products', getProducts);
+router.get("/products/:productId", getProduct);
 router.get('/cart', getCart);
 router.get('/orders', getOrders)
 router.get('/checkout', getCheckout);
