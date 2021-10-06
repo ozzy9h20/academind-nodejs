bedaff3522188612df1fda34eae719cdd952d747
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-10-06 08:04:57 +0700

    Add orders module init

--- routes/shop.js
+++ routes/shop.js
@@ -4,12 +4,14 @@ const {
   getIndex,
   getCart,
   getCheckout,
+  getOrders,
 } = require("../controllers/shop");
 const router = express.Router();
 
 router.get('/', getIndex);
 router.get('/products', getProducts);
 router.get('/cart', getCart);
+router.get('/orders', getOrders)
 router.get('/checkout', getCheckout);
 
 module.exports = router;
