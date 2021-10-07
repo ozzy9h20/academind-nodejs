142e90e589a26f401fb265cfeb99930180d8b2f8
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-10-07 23:16:11 +0700

    Defining product model

--- models/product.js
+++ models/product.js
@@ -1,39 +1,26 @@
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, price, description) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.price = price;
-    this.description = description;
-  }
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-  save() {
-    const { title, price, description, imageUrl } = this;
-    const qs = `INSERT INTO products 
-                VALUES (null, ?, ?, ?, ?)`;
-    return db.execute(qs, [title, price, description, imageUrl]);
-  }
-
-  static fetchAll() {
-    const qs = `SELECT * 
-                FROM products`;
-    return db.execute(qs);
-  }
-
-  static findById(id) {
-    const qs = `SELECT * 
-                FROM products 
-                WHERE products.id = ?`;
-    return db.execute(qs, [id]);
-  }
-
-  static deleteById(id) {
-    const qs = `DELETE
-                FROM products 
-                WHERE id = ?`;
-    return db.execute(qs, [id]);
-  }
-}
+module.exports = Product;
