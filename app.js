435bc7d4f0177af6ab4de281b217c51d373cf506
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-10-09 10:55:19 +0700

    Sequelize - One-to-Many & Many-to-Many relation

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const { get404 } = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -39,6 +41,10 @@ Product.belongsTo(User, {
   onDelete: 'CASCADE',
 });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   .sync()
